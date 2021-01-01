d67fb860032172d0d86e596a36dbc6fad559485d
anthony ervin <ajervin1@asu.edu>
2021-01-01 15:45:03 -0700

    'r'

--- views/ShowClip.vue
+++ views/ShowClip.vue
@@ -22,7 +22,7 @@
 		},
 		computed: {
 			embed_url () {
-				return this.url + this.$route.params.slug + '&parent=https://twitchy.netlify.app'
+				return this.url + this.$route.params.slug + '&parent=https://twitchy.netlify.app/'
 			},
 			clip () {
 				return this.$store.state.clips.find(clip => clip.slug === this.$route.params.slug)
